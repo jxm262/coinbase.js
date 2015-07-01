a309646b96755a471f0a3851084ce07889f49883
Justin Maat <justin.maat@wendys.com>
2015-06-30 23:53:21 -0400

    -adding more unit tests -refactoring createAccessSign to use reqType param

--- test/requestUtil.test.js
+++ test/requestUtil.test.js
@@ -5,7 +5,9 @@ var requestUtil = require('../lib/requestUtil')
     , chai = require('chai')
     , sinonChai = require('sinon-chai')
     , should = chai.should()
-    , _ = require('lodash');
+    , _ = require('lodash')
+    , crypto = require('crypto')
+;
 
 chai.use(sinonChai);
 
@@ -34,6 +36,55 @@ describe('requestUtil', function () {
             requestUtil.buildSubUrl(api, params);
             params.should.not.include.keys('test_key');
         });
+    });
+
+    describe('createAccessSign function', function () {
+        var reqType, timestamp, subUrl, mockHmac, that;
+
+        before(function () {
+            that = this;
+            that._secret = 'testSecret';
+            reqType = 'TEST_TYPE';
+            timestamp = 1;
+            subUrl = 'testSubUrl';
+        });
+
+        beforeEach(function () {
+            mockHmac = crypto.createHmac('sha256', 'test');
+            sandbox.stub(crypto, 'createHmac').returns(mockHmac);
+        });
+
+        it('should return the updated hmac digested in base64', function () {
+            var digestStub = sandbox.stub(mockHmac, 'digest').returns('test');
+
+            var expectedHmac = requestUtil.createAccessSign.call(that, reqType, timestamp, subUrl);
+
+            digestStub.should.have.been.calledWith('base64');
+            expectedHmac.should.equal('test');
+        });
+
+        describe('with empty params', function () {
+            it('should call hmac .update with concatenated timestamp + reqType + subUrl', function () {
+                var params = {};
+                var updateSpy = sandbox.spy(mockHmac, 'update');
+
+                requestUtil.createAccessSign.call(that, reqType, timestamp, subUrl, params);
+
+                updateSpy.should.have.been.calledWith(timestamp + reqType + subUrl);
+            });
+        });
+
+        describe('with non-empty params', function () {
+            it('should call hmac .update with concatenated timestamp + reqType + subUrl + JSON.stringify(params)', function () {
+                var params = {hello: 'world'};
+                var updateSpy = sandbox.spy(mockHmac, 'update');
+
+                requestUtil.createAccessSign.call(that, reqType, timestamp, subUrl, params);
+
+                updateSpy.should.have.been.calledWith(timestamp + reqType + subUrl + JSON.stringify(params));
+            });
+        });
+
     });
 
 });
